d840974668c210915a8dce1a92701a0a61a0e8e4
bernardtran99 <bernardtran99@gmail.com>
2020-05-15 20:13:11 -0400

    Added Clock

--- finalproject.srcs/constrs_1/new/eth.xdc
+++ finalproject.srcs/constrs_1/new/eth.xdc
@@ -1,6 +1,6 @@
 ## Clock signal
-set_property -dict { PACKAGE_PIN E3    IOSTANDARD LVCMOS33 } [get_ports { CLK50MHZ }]; #IO_L12P_T1_MRCC_35 Sch=clk100mhz
-create_clock -add -name sys_clk_pin -period 10.00 -waveform {0 5} [get_ports {CLK50MHZ}];
+set_property -dict { PACKAGE_PIN E3    IOSTANDARD LVCMOS33 } [get_ports { clk_in }]; #IO_L12P_T1_MRCC_35 Sch=clk100mhz
+create_clock -add -name sys_clk_pin -period 10.00 -waveform {0 5} [get_ports {clk_in}];
 
 ## SMSC Ethernet PHY
 set_property -dict { PACKAGE_PIN C9    IOSTANDARD LVCMOS33 } [get_ports { ETH_MDC }]; #IO_L11P_T1_SRCC_16 Sch=eth_mdc
--- finalproject.srcs/sources_1/new/eth.vhd
+++ finalproject.srcs/sources_1/new/eth.vhd
@@ -3,18 +3,40 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity eth is
     port (
-        CLK50MHZ : in std_logic;
+        -- all ports that are connected to ethernet module
+        -- some may not be used
+        clk_in : IN STD_LOGIC;
         ETH_MDC : out std_logic;
         ETH_MDIO : inout std_logic;
         ETH_RSTN : out std_logic;
-        ETH_CRSDV : inout std_logic;--bidirectional
-        ETH_RXERR : 
+        ETH_CRSDV : in std_logic;--bidirectional
+        ETH_RXERR : in std_logic;
+        ETH_RXD : in std_logic_vector (1 downto 0);
+        ETH_TXEN : out std_logic;
+        ETH_TXD : out std_logic_vector (1 downto 0);
+        ETH_REFCLK : out std_logic;
+        ETH_INTN : inout std_logic
         );
 end eth;
 
 architecture Behavioral of eth is
-
+    
+    -- clock component
+    component clk_wiz_0 is
+    port (
+      clk_in1  : in std_logic;
+      clk_out1 : out std_logic
+    );
+    end component;
+    
 begin
-
-
+    
+    
+    -- init clock for ETH_REFCLK
+    clk_wiz_0_inst : clk_wiz_0
+    port map (
+      clk_in1 => clk_in,
+      clk_out1 => ETH_REFCLK
+    );
+    
 end Behavioral;
